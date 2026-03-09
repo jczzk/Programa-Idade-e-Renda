43c1f3359e11b3a29c7f5eea495b0e93bdaca10d
jczzk <santosjaciel710@icloud.com>
2026-03-09 15:17:56 -0300

    Add files via upload

---
+++ Desconto&Renda.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+
+    int idade;
+    float renda;
+
+    printf("Digite a sua idade: ");
+    scanf("%d", &idade);
+    printf("Digite a sua renda mensal: ");
+    scanf("%f", &renda);
+
+    if (idade <= 18 || idade >= 60)
+    {
+    if (renda < 2000) {
+        printf("Voce tem direito ao desconto!\n");
+    } else {
+        printf("Voce nao tem direito ao desconto devido a renda!\n");
+    }
+        
+    } else {
+        printf("Voce nao possui tem direito ao desconto devido idade!\n");
+    }
+
+}
